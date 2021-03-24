651a3c0e1c3e225fa04717f852185601364f09b8
Irving Neyra <irving-bautista98@hotmail.com>
2021-03-23 20:21:06 -0600

    Create and update text model

--- SharpDressMan/src/app/main/main.component.ts
+++ SharpDressMan/src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { NotificationsService } from 'angular2-notifications';
 import { API, Auth, Storage } from 'aws-amplify';
 
 @Component({
@@ -19,13 +20,18 @@ import { API, Auth, Storage } from 'aws-amplify';
 export class MainComponent implements OnInit {
   @Input() username: string | undefined;
   public userData;
+  public costumNotifi = {
+    timeOut: 3000,
+    showProgressBar: true,
+    pauseOnHover: true
+  };
 
-  constructor() { }
+  constructor(private service: NotificationsService ) {  }
 
   async ngOnInit() {
     API.get('sdmApiTest', '/demo', {}).then(responde => {
       console.log(responde);
-    })
+    });
     this.userData = await Auth.currentUserInfo();  
   }
 
@@ -51,11 +57,26 @@ export class MainComponent implements OnInit {
         await Storage.put(key, files[i], {
           // contentType: 'image/png' // contentType is optional
         });
-        console.log("Listoo subido");
+        this.service.success("Carga completa", "Imagen subida con éxito", this.costumNotifi);
       } catch (err) {
         console.log('Error uploading file: ', err);
+        this.service.error("Hubo un error", err, this.costumNotifi);
       }  
     }
   }
 
+  uploadText(){
+    let text = (<HTMLInputElement>document.getElementById('textData')).value;
+    if(text.length > 0 ) {
+      API.post('sdmApiTest','/text',{
+        body: text
+      }).then(responde => {
+        console.log(responde);
+      }).catch(error => {
+        this.service.error("Hubo un error", error, this.costumNotifi);
+      });
+    } else {
+      this.service.error("Hubo un error", "Falta ingresar texto", this.costumNotifi);
+    }
+  }
 }
