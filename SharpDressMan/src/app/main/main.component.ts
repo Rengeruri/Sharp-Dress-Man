6c06c3de5f81686c68b8580b8a514435648c0dbe
Irving Neyra <irving-bautista98@hotmail.com>
2021-04-20 09:26:58 -0500

    added sample event text

--- SharpDressMan/src/app/main/main.component.ts
+++ SharpDressMan/src/app/main/main.component.ts
@@ -26,11 +26,11 @@ export class MainComponent implements OnInit {
     pauseOnHover: true
   };
   public eventsExamples = [
-    {text:"Texto de ejemplo 1", urlAvatar: "https://i.pinimg.com/originals/54/35/8f/54358f662fcaa975582b0be0388befe6.jpg", name:"John Santana"},
-    {text:"Texto de ejemplo 2", urlAvatar: "https://64.media.tumblr.com/e2de105c22f705fa0bfe0567155445d5/tumblr_o7mzm2ucGQ1v4d8dyo1_1280.jpg", name:"Mario Ortiz"},
-    {text:"Texto de ejemplo 3", urlAvatar: "https://i.pinimg.com/280x280_RS/8f/c6/b6/8fc6b6364c319b9881b184baf7f8889d.jpg", name:"Julio Martinez"},
-    {text:"Texto de ejemplo 4", urlAvatar: "https://amigosgringos.com/wp-content/uploads/avatars/149/5b74e33d30a27-bpfull.jpg", name:"Jesus Bautista"},
-    {text:"Texto de ejemplo 5", urlAvatar: "https://i.pinimg.com/originals/58/b8/eb/58b8eb55a97dec538fffb7c4c913000c.jpg", name:"Arturo Saavedra"},
+    {text:"El día de ayer falleció el primo de un amigo, así que mañana iremos a su funeral.", urlAvatar: "https://amigosgringos.com/wp-content/uploads/avatars/149/5b74e33d30a27-bpfull.jpg", name:"John Santana"},
+    {text:"Esta semana tengo varias cenas con varios amigos y no sé qué usar.", urlAvatar: "https://64.media.tumblr.com/e2de105c22f705fa0bfe0567155445d5/tumblr_o7mzm2ucGQ1v4d8dyo1_1280.jpg", name:"Mario Ortiz"},
+    {text:"Mi prima me invito a la fiesta por la boda de una de sus conocidas.", urlAvatar: "https://i.pinimg.com/280x280_RS/8f/c6/b6/8fc6b6364c319b9881b184baf7f8889d.jpg", name:"Julio Martinez"},
+    {text:"A fin de año tendremos nuestra graduación por terminar la universidad.", urlAvatar: "https://i.pinimg.com/originals/54/35/8f/54358f662fcaa975582b0be0388befe6.jpg", name:"Jesus Bautista"},
+    {text:"Me estoy preparando para ir a la carrera que habrá cerca de mi ciudad.", urlAvatar: "https://i.pinimg.com/originals/58/b8/eb/58b8eb55a97dec538fffb7c4c913000c.jpg", name:"Arturo Saavedra"},
   ];
 
   constructor(private service: NotificationsService ) {  }
