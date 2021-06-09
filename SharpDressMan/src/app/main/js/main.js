f8bc538fd2e0d53679e201a7215f446ea8ecff85
Irving Neyra <irving-bautista98@hotmail.com>
2021-06-09 18:58:42 -0500

    VirtualCloset Bug was fixed

--- SharpDressMan/src/app/main/js/main.js
+++ SharpDressMan/src/app/main/js/main.js
@@ -666,51 +666,104 @@
 
     /*----------- Portfolio -----------*/
     $(document).ready(function() {
-        (function() {
-            var grid = $('.portfolio-area .portfolio-grid');
-            var filters = $('.portfolio-area .filter-control li');
-
-            grid.isotope({
-                itemSelector: '.single-item',
-            });
-            filters.on('click', function() {
-                filters.removeClass('tab-active');
-                $(this).addClass('tab-active');
-                var selector = $(this).data('filter');
-                grid.isotope({
-                    filter: selector,
-                    transitionDuration: '.25s'
-                });
-            });
-        }())
-
-        $('.portfolio-area .portfolio-grid .portfolio-item').each(function() {
-            var element = $(this);
-            var target = element.attr('href');
-            $(element).animatedModal({
-                animatedIn: 'fadeIn',
-                animatedOut: 'fadeOut',
-                animationDuration: '.15s',
-                beforeOpen: function() {
-                    $(target + '.lightbox-wrapper .lightbox-gallery').owlCarousel({
-                        loop: true,
-                        margin: 10,
-                        nav: false,
-                        items: 1,
-                        smartSpeed: 400
+        var flag = 0;
+        var checkExist = setInterval(function() {
+            if ($(".single-item").length && flag == 0) {
+                console.log("Si hay ropa");
+                flag = 1;
+                (function() {
+                    var grid = $('.portfolio-area .portfolio-grid');
+                    var filters = $('.portfolio-area .filter-control li');
+                    grid.isotope({
+                        itemSelector: '.single-item',
                     });
-                },
-                afterClose: function() {
-                    $(target + '.lightbox-wrapper .lightbox-gallery').trigger('destroy.owl.carousel');
-                }
-            });
+                    filters.on('click', function() {
+                        filters.removeClass('tab-active');
+                        $(this).addClass('tab-active');
+                        var selector = $(this).data('filter');
+                        grid.isotope({
+                            filter: selector,
+                            transitionDuration: '.25s'
+                        });
+                    });
+                }())
 
-        });
+                $('.portfolio-area .portfolio-grid .portfolio-item').each(function() {
+                    var element = $(this);
+                    var target = element.attr('href');
+                    $(element).animatedModal({
+                        animatedIn: 'fadeIn',
+                        animatedOut: 'fadeOut',
+                        animationDuration: '.15s',
+                        beforeOpen: function() {
+                            $(target + '.lightbox-wrapper .lightbox-gallery').owlCarousel({
+                                loop: true,
+                                margin: 10,
+                                nav: false,
+                                items: 1,
+                                smartSpeed: 400
+                            });
+                        },
+                        afterClose: function() {
+                            $(target + '.lightbox-wrapper .lightbox-gallery').trigger('destroy.owl.carousel');
+                        }
+                    });
 
-        setTimeout(() => {
-            $('.list-inline.filter-control').children().first().click();
-        }, 1000);
+                });
 
+                setTimeout(() => {
+                    $('.list-inline.filter-control').children().first().click();
+                }, 1000);
+            }
+        }, 100)
     });
+
+    // $(document).ready(function() {
+    //     (function() {
+    //         var grid = $('.portfolio-area .portfolio-grid');
+    //         var filters = $('.portfolio-area .filter-control li');
+
+    //         grid.isotope({
+    //             itemSelector: '.single-item',
+    //         });
+    //         filters.on('click', function() {
+    //             filters.removeClass('tab-active');
+    //             $(this).addClass('tab-active');
+    //             var selector = $(this).data('filter');
+    //             grid.isotope({
+    //                 filter: selector,
+    //                 transitionDuration: '.25s'
+    //             });
+    //         });
+    //     }())
+
+    //     $('.portfolio-area .portfolio-grid .portfolio-item').each(function() {
+    //         var element = $(this);
+    //         var target = element.attr('href');
+    //         $(element).animatedModal({
+    //             animatedIn: 'fadeIn',
+    //             animatedOut: 'fadeOut',
+    //             animationDuration: '.15s',
+    //             beforeOpen: function() {
+    //                 $(target + '.lightbox-wrapper .lightbox-gallery').owlCarousel({
+    //                     loop: true,
+    //                     margin: 10,
+    //                     nav: false,
+    //                     items: 1,
+    //                     smartSpeed: 400
+    //                 });
+    //             },
+    //             afterClose: function() {
+    //                 $(target + '.lightbox-wrapper .lightbox-gallery').trigger('destroy.owl.carousel');
+    //             }
+    //         });
+
+    //     });
+
+    //     setTimeout(() => {
+    //         $('.list-inline.filter-control').children().first().click();
+    //     }, 1000);
+
+    // });
 
 }(jQuery));
